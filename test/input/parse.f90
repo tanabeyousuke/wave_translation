088e352b651161528ca0b4a6f917faf1b01ba858
tanabe <tanabe@tanabe>
2025-09-19 12:35:12 +0900

    此処は神代も聞かぬ真秀場

--- test/input/parse.f90
+++ test/input/parse.f90
@@ -14,12 +14,12 @@ module parse !パーサです。シンセサイザの設定や演奏の実行な
   
   type::env
      real env_unit(4)
-     integer out
+     real out
   end type env
   
   type::lfo
      real lfo_unit(3)
-     integer out
+     real out
   end type lfo
   
   type::setting
@@ -47,6 +47,42 @@ contains
     end if
   end function num_reg
   
+  subroutine reallocate_real_2d(list, new_x, new_y)
+    real, allocatable, intent(inout)::list(:, :)
+    integer new_x, new_y
+
+    real, allocatable::buffer(:, :)
+
+    allocate(buffer(new_x, new_y), mold=list)
+
+    list = buffer
+    deallocate(buffer)
+  end subroutine reallocate_real_2d
+
+  subroutine reallocate_env(list, new_size)
+    type(env), allocatable, intent(inout)::list(:)
+    integer, intent(in)::new_size
+
+    type(env), allocatable::buffer(:)
+
+    allocate(buffer(new_size), mold=list)
+
+    list = buffer
+    deallocate(buffer)
+  end subroutine reallocate_env
+
+  subroutine reallocate_lfo(list, new_size)
+    type(lfo), allocatable, intent(inout)::list(:)
+    integer, intent(in)::new_size
+
+    type(lfo), allocatable::buffer(:)
+
+    allocate(buffer(new_size), mold=list)
+
+    list = buffer
+    deallocate(buffer)
+  end subroutine reallocate_lfo
+ 
   subroutine synth_setting(unit_num, set)
     integer, intent(inout)::unit_num
     type(setting),intent(out)::set
@@ -92,15 +128,13 @@ contains
           end if
        end do
 
-       print *, line(ophead:optail)
        operate = line(ophead:optail)
        select case(trim(operate))
        case("osc")
           rgx(1) = set%oscillator%num + 1
           set%oscillator%num = rgx(1)
           if (allocated(set%oscillator%list)) then
-
-             allocate(set%oscillator%list(rgx(1), 2), source=set%oscillator%list)
+             call reallocate_real_2d(set%oscillator%list, rgx(1), 2)
           else
              allocate(set%oscillator%list(rgx(1), 2))
           end if
@@ -120,7 +154,6 @@ contains
              end if
           end do
 
-          print *, line(ophead:optail)
           operate = trim(line(ophead:optail))
           select case(operate)
           case("sin")
@@ -150,28 +183,30 @@ contains
              end if
           end do
 
-          print *, line(ophead:optail)
           operate = trim(line(ophead:optail))
           set%oscillator%list(rgx(1),2) = num_reg(operate)
 
        case("flt")
-          print *, "flt"
           rgx(1) = set%filter%num + 1
           set%filter%num = rgx(1)
-          if (allocated(set%oscillator%list)) then
-             allocate(set%oscillator%list(rgx(1), 2), source=set%oscillator%list)
+          if (allocated(set%filter%list)) then
+             call reallocate_real_2d(set%filter%list, rgx(1), 3)
           else
-             allocate(set%oscillator%list(rgx(1), 2))
+             allocate(set%filter%list(rgx(1), 3))
           end if
 
-          do i = optail, scpos
-             if(line(i:i) /= ' ') then
+          do i = optail + 1, scpos
+             if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
                 ophead = i
+                exit
              end if
-             
+          end do
+          
+          do i = ophead, scpos
              lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-             if(ophead < i .and. lrgx(1)) then
-                optail = i
+             if( lrgx(1)) then
+                optail = i - 1
+                exit
              end if
           end do
 
@@ -186,40 +221,151 @@ contains
           set%filter%list(rgx(1),1) = rgx(2)
 
           do ro = 1, 2
-             do i = optail, scpos
-                if(line(i:i) /= ' ') then
+             do i = optail + 1, scpos
+                if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
                    ophead = i
+                   exit
                 end if
-                
+             end do
+             
+             do i = ophead, scpos
                 lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-                if(ophead < i .and. lrgx(1)) then
-                   optail = i
+                if( lrgx(1)) then
+                   optail = i - 1
+                   exit
                 end if
              end do
              
              operate = trim(line(ophead:optail))
              set%filter%list(rgx(1),ro + 1) = num_reg(operate)
           end do
+
+       case("env")
+           do i = optail + 1, scpos
+             if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
+                ophead = i
+                exit
+             end if
+          end do
+          
+          do i = ophead, scpos
+             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
+             if( lrgx(1)) then
+                optail = i - 1
+                exit
+             end if
+          end do
+
+          operate = trim(line(ophead:optail))
+          read(operate, *) rgx(2)
+          rgx(1) = set%env_num
+          if(rgx(1) < rgx(2)) then
+             set%env_num = rgx(1) + 1
+             rgx(1) = set%env_num
+             if (allocated(set%oscillator%list)) then
+                call reallocate_env(set%envelope, rgx(1))
+             else
+                allocate(set%envelope(rgx(1)))
+             end if
+          end if
+
+          do i = optail + 1, scpos
+             if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
+                ophead = i
+                exit
+             end if
+          end do
+          
+          do i = ophead, scpos
+             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
+             if( lrgx(1)) then
+                optail = i - 1
+                exit
+             end if
+          end do
+
+          operate = trim(line(ophead:optail))
+
+          select case(operate)
+          case("atk")
+             rgx(2) = 1
+          case("dec")
+             rgx(2) = 2
+          case("sus")
+             rgx(2) = 3
+          case("sus")
+             rgx(2) = 4
+          case("out")
+             rgx(2) = 5
+          end select
+
+          do i = optail + 1, scpos
+             if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
+                ophead = i
+                exit
+             end if
+          end do
+          
+          do i = ophead, scpos
+             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
+             if( lrgx(1)) then
+                optail = i - 1
+                exit
+             end if
+          end do
+
+          operate = trim(line(ophead:optail))
+
+          if(rgx(2) /= 5) then
+             set%envelope(rgx(1))%env_unit(rgx(2)) = num_reg(operate)
+          else
+             set%envelope(rgx(1))%out = num_reg(operate)
+          end if
+           
+       case("lfo")
+          rgx(1) = set%lfo_num + 1
+          set%lfo_num = rgx(1)
+          if (allocated(set%lfo)) then
+             call reallocate_lfo(set%lfo, rgx(1))
+          else
+             allocate(set%lfo(rgx(1))
+          end if
+
+          do i = optail + 1, scpos
+             if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
+                ophead = i
+                exit
+             end if
+          end do
+          
+          do i = ophead, scpos
+             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
+             if( lrgx(1)) then
+                optail = i - 1
+                exit
+             end if
+          end do
+          
+          operate = trim(line(ophead:optail))
+
+          select case(operate)
+          case("sin")
+             rgx(2) = 1
+          case("del")
+             rgx(2) = 2
+          case("saw")
+             rgx(2) = 3
+          case("sqr")
+             rgx(2) = 4
+          case("rnd")
+             rgx(2) = 5
+          end select
+          
+          set%lfo(rgx(1))%lfo_unit(1)
+          
+
        end select
-
-
-       ! case(env)
-       !    do i = optail, scpos
-       !       if(line(i:i) /= ' ') then
-       !          ophead = i
-       !       end if
-             
-       !       lrgx(1) = line(i:i) == ' ' or line(i:i) == ';'
-       !       if(ophead < i .and. lrgx) then
-       !          optail = i
-       !       end if
-       !    end do
           
-          
-       !    operate = trim(line(ophead:optail))
-       !    set%filter%list(rgx(1):ro + 1) = num_reg(operate)
-          
-          ! read(operate, *) rgx
     end do
   end subroutine synth_setting
 
