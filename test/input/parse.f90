f5566404b6eafe8dfd2f213a224e74d58b43eb46
tanabe <tanabe@tanabe>
2025-09-28 20:03:41 +0900

    でぎだぁ゛！

--- test/input/parse.f90
+++ test/input/parse.f90
@@ -2,37 +2,35 @@ module parse !パーサです。シンセサイザの設定や演奏の実行な
   implicit none
      
   type::oscillator
-     integer,allocatable::f(:)!波形
-     real,allocatable::g(:)!オシレータのゲイン
-     integer n
+     integer f!波形
+     real g!オシレータのゲイン
   end type oscillator
  
   type::filter
-     integer, allocatable::t(:)!フィルタの種類
-     real, allocatable::f(:)!カットオフ周波数
-     real, allocatable::r(:)!レゾナンス
-     integer n
+     integer t!フィルタの種類
+     real f!カットオフ周波数
+     real r!レゾナンス
   end type filter
   
-  type::env
+  type::envelope
      real env_unit(4)
      real out
-  end type env
+  end type envelope
   
   type::lfo
-     real lfo_unit(3)
-     real out
+     real lfo_unit(4)
   end type lfo
   
   type::setting
-     type(oscillator) osc
-     type(filter) flt
-     type(env), allocatable::envelope(:)
+     type(oscillator), allocatable::osc(:)
+     type(filter), allocatable::flt(:)
+     type(envelope), allocatable::env(:)
      type(lfo), allocatable::lfo(:)
      integer osc_n
      integer flt_n
      integer env_n
      integer lfo_n
+     real amp
   end type setting
   
 contains
@@ -50,409 +48,258 @@ contains
        num_reg = num
     end if
   end function num_reg
-  
-  subroutine module_num_set(unit_num, set)
-    integer unit_num
-    type(setting) set
+
+  subroutine get_token(line, head, tail, scpos)
+    character(len=80),intent(in)::line
+    integer,intent(in)::scpos
+    integer,intent(inout)::head, tail
+    
+    integer ophead, optail, i
+
+    do i = tail, scpos
+       if(line(i:i) /= ' ') then
+          ophead = i
+          exit
+       end if
+    end do
+    
+    do i = ophead + 1, scpos
+       if(line(i:i) == ' ' .or. line(i:i) == ';') then
+          optail = i - 1
+          
+          exit
+       end if
+    end do
+
+    head = ophead
+    tail = optail
+  end subroutine get_token
+
+  subroutine module_num_setting(unit_num, set)
+    integer,intent(in)::unit_num
+    type(setting),intent(inout)::set
 
     character(len=80) line
     character(len=5) operate
     integer iostat_value, scpos, setpos, ophead, optail
-    integer i, ro, rgx(5)
+    integer i, ro, rgx(10)
     logical lrgx(5)
+    
     do
        read(unit_num, '(A:)', iostat=iostat_value) line
        
        scpos = index(line, ";")
        setpos = index(line, "setting:")
        
-       if(setpos < scpos) exit
+       if(setpos /= 0) exit
        
-       if(setpos == 0) cycle
-       
-       do i = 1, scpos
-          if(line(i:i) /= ' ') then
-             ophead = i
-             exit
-          end if
-       end do
-       
-       do i = ophead, scpos
-          lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-          if( lrgx(1)) then
-             optail = i - 1
-             exit
-          end if
-       end do
+       if(scpos == 0) cycle
+
+       optail = 1
+       call get_token(line, ophead, optail, scpos)
        
        operate = trim(line(ophead:optail))
+
        select case(operate)
        case("osc")
-          do i = optail + 1, scpos
-             if(line(i:i) /= ' ') then
-                ophead = i
-                exit
-             end if
-          end do
-          
-          do i = ophead, scpos
-             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-             if( lrgx(1)) then
-                optail = i - 1
-                exit
-             end if
-          end do
-          
-          read(line(ophead:optail), *) set%osc_n
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
+          operate = line(ophead:optail)
+          read(operate, *) set%osc_n
 
        case("flt")
-          do i = optail + 1, scpos
-             if(line(i:i) /= ' ') then
-                ophead = i
-                exit
-             end if
-          end do
-          
-          do i = ophead, scpos
-             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-             if( lrgx(1)) then
-                optail = i - 1
-                exit
-             end if
-          end do
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
 
           read(line(ophead:optail), *) set%flt_n
 
        case("env")
-          do i = optail + 1, scpos
-             if(line(i:i) /= ' ') then
-                ophead = i
-                exit
-             end if
-          end do
-          
-          do i = ophead, scpos
-             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-             if( lrgx(1)) then
-                optail = i - 1
-                exit
-             end if
-          end do
-          
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
+
           read(line(ophead:optail), *) set%env_n
 
        case("lfo")
-          do i = optail + 1, scpos
-             if(line(i:i) /= ' ') then
-                ophead = i
-                exit
-             end if
-          end do
-          
-          do i = ophead, scpos
-             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-             if( lrgx(1)) then
-                optail = i - 1
-                exit
-             end if
-          end do
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
 
           read(line(ophead:optail), *) set%lfo_n
        end select
     end do
-  end subroutine module_num_set
+
+  end subroutine module_num_setting
   
   subroutine synth_setting(unit_num, set)
-    integer unit_num
-    type(setting) set
+    integer, intent(in)::unit_num
+    type(setting), intent(inout)::set
     
     character(len=80) line
     character(len=5) operate
     integer iostat_value, scpos, playpos, ophead, optail
-    integer i, ro, rgx(5)
+    integer i, ro, osc_num, flt_num, env_num, lfo_num, rgx(5)
     logical lrgx(5)
-
-    set%osc_n = 0
-    set%flt_n = 0
-    set%env_n = 0
-    set%lfo_n = 0
 
     ophead = 0
 
+    allocate(set%osc(set%osc_n))
+    allocate(set%flt(set%flt_n))
+    allocate(set%env(set%env_n))
+    allocate(set%lfo(set%lfo_n))
+    osc_num = 0
+    flt_num = 0
+    env_num = 0
+    lfo_num = 0
+    
     do
        read(unit_num, '(A:)', iostat=iostat_value)line
-    
+       
        playpos = index(line, "play:")
        if(playpos /= 0) exit
-
+       
        scpos = index(line, ';')
        if(scpos == 0) cycle
-
+       
        lrgx(2) = .false.
-
+       
+       optail = 1
+       call get_token(line, ophead, optail, scpos)
        operate = line(ophead:optail)
+       
+       print *, operate
        select case(trim(operate))
        case("osc")
-          rgx(1) = set%osc%n + 1
-          set%osc%n = rgx(1)
-          if (set%osc%n - 1 /= 0) then
-             call reallocate_integer(set%osc%f, rgx(1))
-             call reallocate_real(set%osc%g, rgx(1))
-          else
-             allocate(set%osc%f(rgx(1)))
-             allocate(set%osc%g(rgx(1)))
-          end if
-          
-          do i = optail + 1, scpos
-             if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
-                ophead = i
-                exit
-             end if
-          end do
-          
-          do i = ophead, scpos
-             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-             if( lrgx(1)) then
-                optail = i - 1
-                exit
-             end if
-          end do
+          osc_num = osc_num + 1
 
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
+          
           operate = trim(line(ophead:optail))
           select case(operate)
           case("sin")
-             rgx(2) = 1
+             rgx(1) = 1
           case("del")
-             rgx(2) = 2
+             rgx(1) = 2
           case("sqr")
-             rgx(2) = 3
+             rgx(1) = 3
           case("saw")
-             rgx(2) = 4
+             rgx(1) = 4
           end select
           
-          set%osc%f(rgx(1)) = rgx(2)
+          set%osc(osc_num)%f = rgx(1)
 
-          do i = optail + 1, scpos
-             if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
-                ophead = i
-                exit
-             end if
-          end do
-          
-          do i = ophead, scpos
-             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-             if( lrgx(1)) then
-                optail = i - 1
-                exit
-             end if
-          end do
-
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
           operate = trim(line(ophead:optail))
-          set%osc%g(rgx(1)) = num_reg(operate)
-          ! print *, set%osc%g(rgx(1)), set%osc%g(rgx(1) - 1)
+          set%osc(osc_num)%g = num_reg(operate)
 
        case("flt")
-          rgx(1) = set%flt%n + 1
-          set%flt%n = rgx(1)
-          if (rgx(1) -  1 /= 0) then
-             call reallocate_integer(set%flt%t, rgx(1))
-             call reallocate_real(set%flt%f, rgx(1))
-             call reallocate_real(set%flt%r, rgx(1))
-          else
-             allocate(set%flt%t(rgx(1)))
-             allocate(set%flt%f(rgx(1)))
-             allocate(set%flt%r(rgx(1)))
-          end if
+          flt_num = flt_num + 1
 
-          do i = optail + 1, scpos
-             if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
-                ophead = i
-                exit
-             end if
-          end do
-          
-          do i = ophead, scpos
-             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-             if( lrgx(1)) then
-                optail = i - 1
-                exit
-             end if
-          end do
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
 
           operate = trim(line(ophead:optail))
           select case(operate)
           case("low")
-             rgx(2) = 1
+             rgx(1) = 1
           case("hig")
-             rgx(2) = 2
+             rgx(1) = 2
           end select
 
-          set%flt%t(rgx(1)) = rgx(2)
+          set%flt(flt_num)%t = rgx(1)
 
-          do i = optail + 1, scpos
-             if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
-                ophead = i
-                exit
-             end if
-          end do
-          
-          do i = ophead, scpos
-             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-             if( lrgx(1)) then
-                optail = i - 1
-                exit
-             end if
-          end do
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
              
           operate = trim(line(ophead:optail))
-          set%flt%f(rgx(1)) = num_reg(operate)
+          set%flt(flt_num)%f = num_reg(operate)
 
-          do i = optail + 1, scpos
-             if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
-                ophead = i
-                exit
-             end if
-          end do
-          
-          do i = ophead, scpos
-             lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-             if( lrgx(1)) then
-                optail = i - 1
-                exit
-             end if
-          end do
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
              
           operate = trim(line(ophead:optail))
-          set%flt%r(rgx(1)) = num_reg(operate)
+          set%flt(flt_num)%r = num_reg(operate)
+ 
+       case("env")
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
           
-
-       ! case("env")
-       !     do i = optail + 1, scpos
-       !       if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
-       !          ophead = i
-       !          exit
-       !       end if
-       !    end do
+          operate = trim(line(ophead:optail))
+          read(operate, *) rgx(1)
+          if(env_num < rgx(1)) then
+             env_num = env_num + 1 
+          end if
           
-       !    do i = ophead, scpos
-       !       lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-       !       if( lrgx(1)) then
-       !          optail = i - 1
-       !          exit
-       !       end if
-       !    end do
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
 
-       !    operate = trim(line(ophead:optail))
-       !    read(operate, *) rgx(2)
-       !    rgx(1) = set%env_num
-       !    if(rgx(1) < rgx(2)) then
-       !       set%env_num = rgx(1) + 1
-       !       rgx(1) = set%env_num
-       !       if (allocated(set%oscillator%list)) then
-       !          call reallocate_env(set%envelope, rgx(1))
-       !       else
-       !          allocate(set%envelope(rgx(1)))
-       !       end if
-       !    end if
+          operate = trim(line(ophead:optail))
 
-       !    do i = optail + 1, scpos
-       !       if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
-       !          ophead = i
-       !          exit
-       !       end if
-       !    end do
-          
-       !    do i = ophead, scpos
-       !       lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-       !       if( lrgx(1)) then
-       !          optail = i - 1
-       !          exit
-       !       end if
-       !    end do
+          select case(operate)
+          case("atk")
+             rgx(1) = 1
+          case("dec")
+             rgx(1) = 2
+          case("sus")
+             rgx(1) = 3
+          case("rel")
+             rgx(1) = 4
+          case("out")
+             rgx(1) = 5
+          end select
 
-       !    operate = trim(line(ophead:optail))
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
 
-       !    select case(operate)
-       !    case("atk")
-       !       rgx(2) = 1
-       !    case("dec")
-       !       rgx(2) = 2
-       !    case("sus")
-       !       rgx(2) = 3
-       !    case("rel")
-       !       rgx(2) = 4
-       !    case("out")
-       !       rgx(2) = 5
-       !    end select
+          operate = trim(line(ophead:optail))
 
-       !    do i = optail + 1, scpos
-       !       if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
-       !          ophead = i
-       !          exit
-       !       end if
-       !    end do
-          
-       !    do i = ophead, scpos
-       !       lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-       !       if( lrgx(1)) then
-       !          optail = i - 1
-       !          exit
-       !       end if
-       !    end do
-
-       !    operate = trim(line(ophead:optail))
-
-       !    if(rgx(2) /= 5) then
-       !       set%envelope(rgx(1))%env_unit(rgx(2)) = num_reg(operate)
-       !    else
-       !       set%envelope(rgx(1))%out = num_reg(operate)
-       !    end if
+          if(rgx(1) < 5) then
+             set%env(env_num)%env_unit(rgx(1)) = num_reg(operate)
+          else
+             set%env(env_num)%out = num_reg(operate)
+          end if
            
-       ! case("lfo")
-       !    rgx(1) = set%lfo_num + 1
-       !    set%lfo_num = rgx(1)
-       !    if (allocated(set%lfo)) then
-       !       call reallocate_lfo(set%lfo, rgx(1))
-       !    else
-       !       allocate(set%lfo(rgx(1)))
-       !    end if
+       case("lfo")
+          lfo_num = lfo_num + 1
 
-       !    do i = optail + 1, scpos
-       !       if(line(i:i) /= ' ' .and. (lrgx(2) .eqv. .false.)) then
-       !          ophead = i
-       !          exit
-       !       end if
-       !    end do
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
           
-       !    do i = ophead, scpos
-       !       lrgx(1) = line(i:i) == ' ' .or. line(i:i) == ';'
-       !       if( lrgx(1)) then
-       !          optail = i - 1
-       !          exit
-       !       end if
-       !    end do
-          
-       !    operate = trim(line(ophead:optail))
+          operate = trim(line(ophead:optail))
 
-       !    select case(operate)
-       !    case("sin")
-       !       rgx(2) = 1
-       !    case("del")
-       !       rgx(2) = 2
-       !    case("saw")
-       !       rgx(2) = 3
-       !    case("sqr")
-       !       rgx(2) = 4
-       !    case("rnd")
-       !       rgx(2) = 5
-       !    end select
+          select case(operate)
+          case("sin")
+             rgx(1) = 1
+          case("del")
+             rgx(1) = 2
+          case("saw")
+             rgx(1) = 3
+          case("sqr")
+             rgx(1) = 4
+          case("rnd")
+             rgx(1) = 5
+          end select
           
-       !    set%lfo(rgx(1))%lfo_unit(1) = rgx(2)
+          set%lfo(lfo_num)%lfo_unit(1) = rgx(1)
 
+          do i = 2, 4
+             optail = optail + 1
+             call get_token(line, ophead, optail, scpos)
+             operate = line(ophead:optail)
+
+             set%lfo(lfo_num)%lfo_unit(i) = num_reg(operate)
+          end do
+          
+       case("amp")
+          optail = optail + 1
+          call get_token(line, ophead, optail, scpos)
+          operate = line(ophead:optail)
+          
+          set%amp = num_reg(operate)
        end select
     end do
   end subroutine synth_setting
-
+  
   subroutine execute(filename, unit_num)
     character(*),intent(in)::filename
     integer unit_num
@@ -479,19 +326,9 @@ contains
     
     call module_num_setting(unit_num, set)
 
-    do
-       read(unit_num, '(A:)', iostat=iostat_value) line
-       if(iostat_value /= 0) exit
-       ! print *, line
-
-       setpos = index(line, "setting:")
-       if(setpos /= 0) exit
-    end do
-
     call synth_setting(unit_num, set)
     
     close(unit_num)
   end subroutine execute
-
 
 end module parse
