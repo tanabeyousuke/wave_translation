fe2ee158236337e3363493fc2ea6fc788e166dbf
tanabe <tanabe@tanabe>
2025-09-30 07:40:30 +0900

    env

--- src/fortran/env.f90
+++ src/fortran/env.f90
@@ -22,22 +22,41 @@ contains
     real rgx(2) 
     
     if(push == 1)then
-       if(data%phase == 4 .or. data%phase == 0) data%phase = 1
+       data%start = data%start + 1
+    else
+       data%last = data%last + 1
     end if
-
+    
     select case(data%phase)
+    case(0)
+       if (push == 1) then
+          data%phase = 1
+          env_out = 0
+       end if
     case(1)
-       if(atk > x) data%phase = 2
-       env_out = (100 * x) / data%atk
+       if(atk > data%start) data%phase = 2
+       env_out = (100 * data%start) / data%atk
     case(2)
-       if(dec > x) data%phase = 3
+       if(dec > data%start) data%phase = 3
        rgx(1) = (data%sus - 100) / data%dec
-       env_out = rgx(1) * (x - data%atk) + 100
+       env_out = rgx(1) * (data%start - data%atk) + 100
     case(3)
-       if(push == 0) data%phase = 4
+       if(push == 0) then
+          data%phase = 4
+          data%last = 0
+       end if
        env_out = data%sus
     case(4)
-       if(push == 1) data%phase = 1
-
-       rgx(1) = data%sus / data&rel
-       rgx(2) = rgx(1) * (x - 
+       if(push == 1) then
+          data%phase = 1
+          data%start = 0
+       end if
+       rgx(1) = data%sus / data%rel * -1
+       rgx(2) = rgx(1) * data%last
+       if(rgx(2) <= 0) then
+          data%phase = 0
+       end if
+       env_out = rgx(2)
+    end select
+  end function env_out
+end module env
